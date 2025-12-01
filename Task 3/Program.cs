d64971086c4e46bdd4cf534a8e33a1ed0f3e9dab
Shakya-nishant <155061583+Nishant10011@users.noreply.github.com>
2025-12-01 20:01:17 +0545

    Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_3
+{
+    public class Program
+    {
+        public static void ProcessNumbers(int[] numbers, Func<int, bool> condition, string title)
+        {
+            Console.WriteLine(title);
+            foreach (var num in numbers)
+            {
+                if (condition(num))
+                    Console.WriteLine(num);
+            }
+            Console.WriteLine();
+        }
+        public static void Main()
+        {
+            int[] nums = { 3, 8, 12, 5, 20, 1, 15 };
+            ProcessNumbers(nums, n => n % 2 == 0, "Even numbers:");
+            ProcessNumbers(nums, n => n > 10, "Numbers greater than 10:");
+        }
+    }
+}
